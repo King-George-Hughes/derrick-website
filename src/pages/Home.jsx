0ce981133b6aa2dee3f60cac2b3c9cb24c9eb97c
unknown <hughesgeorgeking@gmail.com>
2023-04-05 22:07:14 +0000

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,12 +12,21 @@ const Home = () => {
           alt=""
           className="absolute h-full left-0 top-0 bottom-0 w-full object-cover bg-cover z-0"
         />
-        <div className="absolute w-full h-full left-0 right-0 bottom-0 bg-gradient-to-t from-gradient1 to-gradient2"></div>
-        <div className="container mx-auto z-10 mt-32">
-          <div className="left-side">
-            <h1>International Currency Transfer Provider</h1>
+        <div className="absolute w-full h-full left-0 right-0 bottom-0 bg-gradient-to-l from-gradient1 to-gradient2"></div>
+
+        <div className="container mx-auto z-10 mt-32 flex flex-col md:flex-row">
+          <div className="left-side w-1/2">
+            <h1 className="text-5xl font-poppins text-white font-bold max-w-lg leading-10">
+              International Currency Transfer Provider
+            </h1>
           </div>
-          <div className="right-side"></div>
+          <div className="right-side">
+            <div className="card bg-gradient-to-r from-primaryColor to-secondaryColor p-4 rounded-xlg">
+              <p>Exchange Rate</p>
+              <h2>1 GBP = 1.29356 USD</h2>
+            </div>
+            <div className="card"></div>
+          </div>
         </div>
       </div>
     </div>
