b2c7e8eb46f74ccbdadfa6c7ada4158de47d0841
unknown <hughesgeorgeking@gmail.com>
2023-04-05 23:06:26 +0000

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div>
       <NavBar />
-      <div className="relative w-full h-screen flex">
+      <div className="relative w-full h-full flex md:h-screen">
         <img
           src={bg}
           alt=""
@@ -16,7 +16,7 @@ const Home = () => {
         />
         <div className="absolute w-full h-full left-0 right-0 bottom-0 bg-gradient-to-l from-gradient1 to-gradient2"></div>
 
-        <div className="container mx-auto z-10 mt-32 flex flex-col md:flex-row">
+        <div className="container mx-auto z-10 mt-32 flex flex-col md:flex-row md:gap-5">
           <div className="left-side">
             <h1 className="text-4xl font-poppins text-white font-medium max-w-2xl leading-none md:text-7xl md:font-bold">
               International Currency Transfer Provider
@@ -73,12 +73,32 @@ const Home = () => {
               <span className="text-xl text-white">See how it works</span>
             </div>
           </div>
-          <div className="mt-10 right-side md:mt-0">
+
+          <div className="mt-10 right-side md:mt-0 md:w-1/3">
             <div className="card bg-gradient-to-r from-primaryColor to-secondaryColor p-4 rounded-xl text-center text-white">
               <p className="text-xl">Exchange Rate</p>
               <h2 className="text-2xl">1 GBP = 1.29356 USD</h2>
             </div>
-            <div className="card"></div>
+            <div className="bg-white p-5">
+              <select name="bank" id="bank" className="w-full p-2 my-5">
+                <option value="bank_transfer">Bank Transfer</option>
+                <option value="bank_transfer">Send Money</option>
+                <option value="bank_transfer">Interest Money</option>
+                <option value="bank_transfer">Invest Money</option>
+              </select>
+              <select name="bank" id="bank" className="w-full p-2 my-5">
+                <option value="bank_transfer">GBP</option>
+                <option value="bank_transfer">USD</option>
+                <option value="bank_transfer">EUR</option>
+                <option value="bank_transfer">YEN</option>
+              </select>
+              <select name="bank" id="bank" className="w-full p-2 my-5">
+                <option value="bank_transfer">USD</option>
+                <option value="bank_transfer">GBP</option>
+                <option value="bank_transfer">EUR</option>
+                <option value="bank_transfer">YEN</option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
