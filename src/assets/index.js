a0e80d02e22c2cfa23842e455a7fe4624fe1baaa
unknown <hughesgeorgeking@gmail.com>
2023-04-06 06:33:49 +0000

    changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,5 @@
 import menu from "./menu.png";
 import close from "./close.png";
 import bg from "./bg.jpg";
-import customers from "./customer.png";
-import datas from "./data.png";
-import profits from "./profits.png";
 
-export { menu, close, bg, customers, datas, profits };
+export { menu, close, bg };
--- src/data/data.js
+++ src/data/data.js
@@ -1,62 +1,62 @@
 export const allCountries = [
   {
     id: 1,
-    img: "../src/assets/countries/img1.png",
+    img: "../countries/img1.png",
     name: "USA",
   },
   {
     id: 2,
-    img: "../src/assets/countries/img2.png",
+    img: "../countries/img2.png",
     name: "UK",
   },
   {
     id: 3,
-    img: "../src/assets/countries/img3.png",
+    img: "../countries/img3.png",
     name: "New Zealand",
   },
   {
     id: 4,
-    img: "../src/assets/countries/img4.png",
+    img: "../countries/img4.png",
     name: "France",
   },
   {
     id: 5,
-    img: "../src/assets/countries/img5.png",
+    img: "../countries/img5.png",
     name: "German",
   },
   {
     id: 6,
-    img: "../src/assets/countries/img6.png",
+    img: "../countries/img6.png",
     name: "Italy",
   },
   {
     id: 7,
-    img: "../src/assets/countries/img7.png",
+    img: "../countries/img7.png",
     name: "Greece",
   },
   {
     id: 8,
-    img: "../src/assets/countries/img8.png",
+    img: "../countries/img8.png",
     name: "China",
   },
   {
     id: 9,
-    img: "../src/assets/countries/img9.png",
+    img: "../countries/img9.png",
     name: "Kuwait",
   },
   {
     id: 10,
-    img: "../src/assets/countries/img10.png",
+    img: "../countries/img10.png",
     name: "Argentina",
   },
   {
     id: 11,
-    img: "../src/assets/countries/img11.png",
+    img: "../countries/img11.png",
     name: "Sweden",
   },
   {
     id: 12,
-    img: "../src/assets/countries/img12.png",
+    img: "../countries/img12.png",
     name: "Thailand",
   },
 ];
@@ -65,21 +65,21 @@ export const howItWorks = [
   {
     id: 1,
     title: "Create a free account",
-    img: "../src/assets/customer.png",
+    img: "../customer.png",
     message:
       "Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh.",
   },
   {
     id: 2,
     title: "Send your funds",
-    img: "../src/assets/profits.png",
+    img: "../profits.png",
     message:
       "Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh.",
   },
   {
     id: 3,
     title: "Track your transfer",
-    img: "../src/assets/data.png",
+    img: "../data.png",
     message:
       "Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh.",
   },
