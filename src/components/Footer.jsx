4995e35a69fe66e908df511b58b04dee31dcb6b8
unknown <hughesgeorgeking@gmail.com>
2023-04-09 01:24:29 +0000

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,17 +10,17 @@ const Footer = () => {
   return (
     <footer className="bg-black pt-8">
       <div className="container mx-auto p-3 text-white flex flex-col items-center">
-        <div className="w-full flex flex-col items-start md:flex-row">
-          <div className="py-6 md:w-1/4">
+        <div className="w-full flex flex-col items-start md:pt-5 md:pb-20 md:flex-row">
+          <div className="py-6 px-2 w-full md:w-1/4">
             <h1 className="text-3xl font-bold mb-5">LOGO</h1>
-            <p className="font-light">
+            <p className="font-light md:max-w-md">
               Quis ipsum suspendisse ultrices gravida commodo. Risus commodo
               veliliee vel viverra maecenas accumsan lacus vel facilisis
             </p>
           </div>
 
-          <div className="py-6 md:w-1/4">
-            <h2 className="text-xl font-medium pb-2">Company</h2>
+          <div className="py-6 px-2 w-full md:w-1/4">
+            <h2 className="text-2xl font-medium pb-2">Company</h2>
             <hr className="w-[50px] h-[2px] border-none bg-primaryColor mb-3" />
             <ul className="space-y-2 text-md font-light">
               <li>
@@ -38,8 +38,8 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="py-6 md:w-1/4">
-            <h2 className="text-xl font-medium pb-2">Supprot</h2>
+          <div className="py-6 px-2 w-full md:w-1/4">
+            <h2 className="text-2xl font-medium pb-2">Supprot</h2>
             <hr className="w-[50px] h-[2px] border-none bg-primaryColor mb-3" />
             <ul className="space-y-2 text-md font-light">
               <li>
@@ -54,11 +54,13 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="py-6 md:w-1/4">
-            <h2 className="text-xl font-medium pb-2">Address</h2>
+          <div className="py-6 px-2 w-full md:w-1/4">
+            <h2 className="text-2xl font-medium pb-2">Address</h2>
             <hr className="w-[50px] h-[2px] border-none bg-primaryColor mb-3" />
             <ul className="space-y-2 text-md font-light">
-              <li>Location: 27 Division St, New York, NY 10002, USA</li>
+              <li className="md:max-w-md">
+                Location: 27 Division St, New York, NY 10002, USA
+              </li>
               <li>
                 Email:
                 <a href="#"> logo@gmail.com</a>
@@ -71,8 +73,9 @@ const Footer = () => {
           </div>
         </div>
 
-        <div>
-          <h4>
+        <div className="w-full text-center mt-5">
+          <hr className="w-full border-none h-[2px] bg-primaryColor" />
+          <h4 className="py-5">
             Copyright @{new Date().getFullYear()} LOGO | All rights reserved.
           </h4>
         </div>
